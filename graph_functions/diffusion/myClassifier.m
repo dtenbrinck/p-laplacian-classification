c5d7e58861f87c8c389d3ae86a125be5a81ec86c
Daniel Tenbrinck <daniel.tenbrinck@fau.de>
2021-09-27 22:51:20 +0200

    Removed unnecessary files and made code much faster due to lower threshold.

--- graph_functions/diffusion/myClassifier.m
+++ graph_functions/diffusion/myClassifier.m
@@ -2,7 +2,7 @@ function fneu = myClassifier(G, f0, p, lambda)
 falt = f0;
 fneu = gaussjacobidiffusion(falt, f0, p, lambda, G);
 diff = norm(fneu-falt);
-while(diff>eps)
+while(diff>1e-3)
 %for i = 1:1
     falt = fneu;
     fneu = gaussjacobidiffusion(falt, f0, p, lambda, G);
--- main.m
+++ main.m
@@ -51,8 +51,10 @@ weightFunction.function = @(x)100*exp(-x.^2./10e+5);
 
 %% PERFORM CLASSIFICATION
 
+tic
 [~,~,perc] = classifyMnist(G,2,10,labels, L,10);
 save('./savedData/goodweights12.mat', 'perc');
+toc
 
 [~,~,perc] = classifyMnist(G,2,10,labels, L,50);
 save('./savedData/goodweights52.mat', 'perc');
