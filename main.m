ba97b39f92933468110ef71d64208e877e9cc0b4
TimRoith <tim.roith@fau.de>
2021-09-27 23:27:12 +0200

    update git ignore

--- main.m
+++ main.m
@@ -13,7 +13,7 @@ addpath('./savedData');
 images = loadMNISTImages('t10k-images.idx3-ubyte');
 
 %%GET LABELS
-[L, labels] = labelgroups('t10k-labels.idx1-ubyte');
+[lGroups, labels] = labelgroups('t10k-labels.idx1-ubyte');
 
 %%SET COORDINATES
 %%each picture's gray values are treated as coordinates in 784-dim space
@@ -49,36 +49,41 @@ weightFunction.function = @(x)100*exp(-x.^2./10e+5);
 %load('badweights.mat', 'Gdis'); % tangent distance with w(x) = 1
 %load('euclidean.mat', 'Ge');  % euclidean distance with 'good' weight function
 
+%% Get labeled Data
+numLabels = 5;
+anz = [980, 1135, 1032, 1010, 982, 892,958,1028,974,1009]';
+labeledIndices = labelIndices(numLabels, anz, lGroups);
+
 %% PERFORM CLASSIFICATION
 
 
 tic
-[~,~,perc] = classifyMnist(G,2,10,labels, L,10);
+[~,~,perc] = classifyMnist(G, 2, 10, labeledIndices);
 save('./savedData/goodweights12.mat', 'perc');
 toc
 
-[~,~,perc] = classifyMnist(G,2,10,labels, L,50);
+[~,~,perc] = classifyMnist(G,2,10,labels, lGroups,50);
 save('./savedData/goodweights52.mat', 'perc');
 
-[l,t,perc] = classifyMnist(G,2,10,labels, L,100);
+[l,t,perc] = classifyMnist(G,2,10,labels, lGroups,100);
 save('./savedData/goodweights102.mat', 'perc');
 save('./savedData/goodweights102table.mat', 't');
 save('./savedData/goodweights102labels.mat', 'l');
 
-[~,~,perc] = classifyMnist(Gdis,2,10,labels, L,10);
+[~,~,perc] = classifyMnist(Gdis,2,10,labels, lGroups,10);
 save('./savedData/badweights12.mat', 'perc');
 
-[~,~,perc] = classifyMnist(Gdis,2,10,labels, L,50);
+[~,~,perc] = classifyMnist(Gdis,2,10,labels, lGroups,50);
 save('./savedData/badweights52.mat', 'perc');
 
-[~,~,perc] = classifyMnist(Gdis,2,10,labels, L,100);
+[~,~,perc] = classifyMnist(Gdis,2,10,labels, lGroups,100);
 save('./savedData/badweights102.mat', 'perc');
 
-[~,~,perc] = classifyMnist(Ge,2,10,labels, L,10);
+[~,~,perc] = classifyMnist(Ge,2,10,labels, lGroups,10);
 save('./savedData/euclidean12.mat', 'perc');
 
-[~,~,perc] = classifyMnist(Ge,2,10,labels, L,50);
+[~,~,perc] = classifyMnist(Ge,2,10,labels, lGroups,50);
 save('./savedData/euclidean52.mat', 'perc');
 
-[~,~,perc] = classifyMnist(Ge,2,10,labels, L,100);
+[~,~,perc] = classifyMnist(Ge,2,10,labels, lGroups,100);
 save('./savedData/euclidean102.mat', 'perc');
